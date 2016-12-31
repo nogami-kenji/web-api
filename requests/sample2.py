b2995057bd19eb41b1b5b42550bdc977e5ab8e0d
nogami-kenji <nogami3333@gmail.com>
2016-12-31 09:33:10 +0900

    sample2 update

--- requests/sample2.py
+++ requests/sample2.py
@@ -37,6 +37,8 @@ def yapi_topics():
     #response = requests.get(url + params, proxies=proxy)
     response = requests.get(url + params)
     #
+    print(response.status_code)
+    print(response.headers)
 
     return response.text
 
